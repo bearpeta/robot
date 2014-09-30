2feffc77e03a948e257a3095e45fcc7a4ddb2f7a
alessandro.petrillo@stud.hslu.ch <alessandro.petrillo@stud.hslu.ch>
2014-09-30 20:38:36 +0200

    SwitchOn/Off gemacht

--- RobotView/SwitchView.cs
+++ RobotView/SwitchView.cs
@@ -6,14 +6,30 @@ using System.Drawing;
 using System.Data;
 using System.Text;
 using System.Windows.Forms;
+using Hslu.Csa.Team6.RobotCtrl.Output;
 
 namespace Hslu.Csa.Team6.RobotView
 {
     public partial class SwitchView : UserControl
     {
+        private bool state;
+
         public SwitchView()
         {
             InitializeComponent();
+        }
+
+        public bool State
+        {
+            get
+            {
+                return this.state;
+            }
+            set
+            {
+                this.state = value;
+                this.pictureBoxSwitch.Image = this.state ? Resources.Resource.SwitchOn : Resources.Resource.SwitchOff;
+            }
         }
     }
 }
