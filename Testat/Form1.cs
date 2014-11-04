f5b5e03cfdf395c5c3d5d86fc457a0af592907df
unknown <ran@RAN-NB.netree.lan>
2014-11-04 13:29:49 +0100

    Testat-Projekt hinzugefügt

---
+++ Testat/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Hslu.Csa.Team6.RobotCtrl;
+using System.Threading;
+
+namespace Hslu.Csa.Team6.Testat
+{
+    public partial class Form1 : Form
+    {
+        Robot robot = new Robot();
+        bool isWorking = false;
+
+        public Form1()
+        {
+            this.InitializeComponent();
+            this.robot.RobotConsole[Switches.Switch1].SwitchStateChanged += this.StartTestatProgram;
+        }
+
+        private void StartTestatProgram(object sender, SwitchEventArgs e)
+        {
+            bool start = false;
+
+            lock (this)
+            {
+                if (!this.isWorking)
+                {
+                    this.isWorking = true;
+                    start = true;
+                }
+            }
+
+            if (start)
+            {
+                // Blinken starten
+                LedBlinker ledBlinker = new LedBlinker(this.robot);
+                Thread ledThread = new Thread(ledBlinker.Blink);
+                ledThread.Start();
+                while (!ledThread.IsAlive) ;
+
+                Thread.Sleep(10000);
+
+                // Stop blinking
+                ledBlinker.RequestStop();
+                ledThread.Join();
+            }
+
+            lock (this)
+            {
+                this.isWorking = false;
+            }
+        }
+    }
+}
---
+++ Testat/LedBlinker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Hslu.Csa.Team6.RobotCtrl;
+using System.Threading;
+
+namespace Hslu.Csa.Team6.Testat
+{
+    internal class LedBlinker
+    {
+        public void Blink(Robot robot)
+        {
+            while (!shouldStop)
+            {
+                bool ledEnabled = true;
+                robot.RobotConsole[Leds.Led1].LedEnabled = ledEnabled;
+                robot.RobotConsole[Leds.Led2].LedEnabled = ledEnabled;
+                robot.RobotConsole[Leds.Led3].LedEnabled = ledEnabled;
+                robot.RobotConsole[Leds.Led4].LedEnabled = ledEnabled;
+
+                ledEnabled = !ledEnabled;
+                Thread.Sleep(1000);
+            }
+            
+            // Terminate Thread
+        }
+
+        public void RequestStop()
+        {
+            shouldStop = true;
+        }
+
+        private volatile bool shouldStop;
+    }
+}
