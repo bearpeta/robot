bb9052749e4c6fdc3ef669da6c9edd3154ffb339
unknown <ran@RAN-NB.netree.lan>
2014-12-02 19:30:25 +0100

    Neue Projekte für Testat2

---
+++ Testat2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hslu.Csa.Team6.Testat2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
