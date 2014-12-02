644073624cc0072680b825fe2d70634a90ae6cba
alessandro.petrillo@stud.hslu.ch <alessandro.petrillo@stud.hslu.ch>
2014-12-02 20:59:31 +0100

    Client GUI erstellt

--- BluetoothClient/ClientForm.Designer.cs
+++ BluetoothClient/ClientForm.Designer.cs
@@ -28,11 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBoxRobots = new System.Windows.Forms.ComboBox();
+            this.textBoxCommands = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.comboBoxActions = new System.Windows.Forms.ComboBox();
+            this.textBoxAngle = new System.Windows.Forms.TextBox();
+            this.textBoxLength = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxRobots
+            // 
+            this.comboBoxRobots.FormattingEnabled = true;
+            this.comboBoxRobots.Location = new System.Drawing.Point(58, 20);
+            this.comboBoxRobots.Name = "comboBoxRobots";
+            this.comboBoxRobots.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxRobots.TabIndex = 2;
+            // 
+            // textBoxCommands
+            // 
+            this.textBoxCommands.Location = new System.Drawing.Point(27, 135);
+            this.textBoxCommands.Multiline = true;
+            this.textBoxCommands.Name = "textBoxCommands";
+            this.textBoxCommands.Size = new System.Drawing.Size(405, 238);
+            this.textBoxCommands.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Robot:";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(357, 87);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // comboBoxActions
+            // 
+            this.comboBoxActions.FormattingEnabled = true;
+            this.comboBoxActions.Location = new System.Drawing.Point(27, 87);
+            this.comboBoxActions.Name = "comboBoxActions";
+            this.comboBoxActions.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxActions.TabIndex = 7;
+            // 
+            // textBoxAngle
+            // 
+            this.textBoxAngle.Location = new System.Drawing.Point(179, 88);
+            this.textBoxAngle.Name = "textBoxAngle";
+            this.textBoxAngle.Size = new System.Drawing.Size(50, 20);
+            this.textBoxAngle.TabIndex = 8;
+            // 
+            // textBoxLength
+            // 
+            this.textBoxLength.Location = new System.Drawing.Point(256, 88);
+            this.textBoxLength.Name = "textBoxLength";
+            this.textBoxLength.Size = new System.Drawing.Size(73, 20);
+            this.textBoxLength.TabIndex = 9;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(179, 389);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 10;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Action";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(179, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Angle";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(256, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Length";
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 424);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxLength);
+            this.Controls.Add(this.textBoxAngle);
+            this.Controls.Add(this.comboBoxActions);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCommands);
+            this.Controls.Add(this.comboBoxRobots);
+            this.Name = "ClientForm";
             this.Text = "ClientForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxRobots;
+        private System.Windows.Forms.TextBox textBoxCommands;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ComboBox comboBoxActions;
+        private System.Windows.Forms.TextBox textBoxAngle;
+        private System.Windows.Forms.TextBox textBoxLength;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
     }
 }
--- BluetoothClient/ClientForm.cs
+++ BluetoothClient/ClientForm.cs
@@ -12,9 +12,47 @@ namespace Hslu.Csa.Team6.BluetoothClient
 {
     public partial class ClientForm : Form
     {
+        #region robotDictionary
+        private Dictionary<String, String> robotDictionary = new Dictionary<string, string>()
+            {
+                {"HSLU_01","001BDC002157"},
+                {"HSLU_02","001BDC0FFDEF"},
+                {"HSLU_03","001BDC0021C4"},
+                {"HSLU_04","001BDC0F8F39"},
+                {"HSLU_05","001BDC00212A"},
+                {"HSLU_06","001BDC0003CC"},
+                {"HSLU_07","001BDC002140"},
+                {"HSLU_08","001BDC0FFEDE"},
+                {"HSLU_09","001BDC0FFE1C"},
+                {"HSLU_10","001BDC000023"},
+                {"HSLU_11","001BDC0021F1"},
+                {"HSLU_12","001BDC002145"},
+                {"HSLU_13","001BDC00034C"},
+                {"HSLU_14","001BDC002139"},
+                {"HSLU_15","001BDC0FF453"},
+                {"HSLU_16","001BDC0022D5"},
+                {"HSLU_17","001BDC0022D6"},
+                {"HSLU_18","001BDC00202F"},
+                {"HSLU_19","001BDC0FFDED"},
+                {"HSLU_20","001BDC0FF360"},
+                {"HSLU_21","001BDC0FE81D"},
+                {"HSLU_25","000000000000"},
+                {"HSLU_30","001BDC0021D1"}
+            };
+        #endregion 
+
+        #region actions
+        private String[] actions = new String[] { 
+            "TrackLine ", 
+            "TrackTurnLeft ", 
+            "TrackTurnRight ", 
+            "TrackArcLeft ", 
+            "TrackArcRight " 
+        };
+        #endregion
+
         public ClientForm()
         {
-
             // check if Bluetooth-Stick is available
             if (BluetoothRadio.IsSupported)
             {
@@ -33,6 +71,45 @@ namespace Hslu.Csa.Team6.BluetoothClient
             }
 
             InitializeComponent();
+
+            comboBoxRobots.DataSource = new BindingSource(robotDictionary, null);
+            comboBoxRobots.DisplayMember = "Key";
+            comboBoxRobots.ValueMember = "Value";
+
+            foreach (String action in actions) {
+                comboBoxActions.Items.Add(action);
+            }
+        }
+
+        public String getSelectedRobotMAC() {
+            return comboBoxRobots.SelectedValue.ToString();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            String action = comboBoxActions.SelectedItem.ToString();
+            String angle = textBoxAngle.Text;
+            String length = textBoxLength.Text;
+
+            if(angle.Equals("")) {
+                textBoxCommands.AppendText(action + length + Environment.NewLine);
+            }
+            else if (length.Equals(""))
+            {
+                textBoxCommands.AppendText(action + angle + Environment.NewLine);
+            }
+            else {
+                textBoxCommands.AppendText(action + angle + " " + length + Environment.NewLine);
+            } 
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            string[] lines = textBoxCommands.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            foreach (String line in lines) {
+                Console.WriteLine(line);
+            }
         }
     }
 }
